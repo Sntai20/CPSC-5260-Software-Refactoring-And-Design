856aa18f738e835e94aefeab9c055260f0e5d4a9
Antonio Santana <ansantan@microsoft.com>
2025-02-08 10:41:51 -0800

    Refactor detection methods to use file contents
    Updated the implementation to read the source file's contents into a string array before passing it to the `DetectLongMethods` and `DetectLongParameterLists` methods. Adjusted the import statement for `CodeSmellDetection` for consistency.

--- CodeSmellDetection/src/Program.cs
+++ CodeSmellDetection/src/Program.cs
@@ -1,11 +1,10 @@
-﻿
-using CodeSmellDetection;
+﻿using CodeSmellDetection;
 
 string filePath = "path/to/your/source/file.cs";
 
 var longMethodDetector = new LongMethodDetector();
 var longParameterListDetector = new LongParameterListDetector();
 
-longMethodDetector.DetectLongMethods(filePath);
+string[]? fileContents = File.ReadAllLines(filePath);
+longMethodDetector.DetectLongMethods(fileContents);
 longParameterListDetector.DetectLongParameterLists(filePath);
-
