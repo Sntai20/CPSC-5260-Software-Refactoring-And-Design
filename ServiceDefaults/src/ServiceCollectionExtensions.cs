64f78b8b1772e55bf9cab564f26cf9640bc23eed
Antonio Santana <ansantan@microsoft.com>
2025-02-17 12:45:13 -0800

    Add OpenTelemetry.Metrics support in extensions
    This commit introduces a using directive for `OpenTelemetry.Metrics;` in the `ServiceCollectionExtensions.cs` file. This addition enables the application to utilize metrics functionality from the OpenTelemetry library, enhancing its observability by allowing the collection and reporting of metrics alongside existing tracing capabilities.

--- ServiceDefaults/src/ServiceCollectionExtensions.cs
+++ ServiceDefaults/src/ServiceCollectionExtensions.cs
@@ -13,7 +13,7 @@ using OpenTelemetry.Trace;
 // Adds common .NET Aspire services: service discovery, resilience, health checks, and OpenTelemetry.
 // This project should be referenced by each service project in your solution.
 // To learn more about using this project, see https://aka.ms/dotnet/aspire/service-defaults
-public static class Extensions
+public static class ServiceCollectionExtensions
 {
     public static TBuilder AddServiceDefaults<TBuilder>(this TBuilder builder)
         where TBuilder : IHostApplicationBuilder
