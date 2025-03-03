25d0ec9693c933ec6c256ea2f544414d8ccabc14
Antonio Santana <ansantan@outlook.com>
2025-03-03 15:44:53 -0800

    Implement combination generation and printing functionality

--- Lecture/src/Program.cs
+++ Lecture/src/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static int[] InitializeCombination(int length)
+{
+    int[] combination = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        combination[i] = i + 1;
+    }
+
+    return combination;
+}
+
+static void PrintCombination(int[] combination)
+{
+    Console.WriteLine(string.Join(" ", combination));
+}
+
+static bool NextCombination(int[] combination, int range)
+{
+    int index = combination.Length - 1;
+    while (index >= 0 && combination[index] == range - (combination.Length - 1 - index))
+    {
+        index--;
+    }
+
+    if (index < 0)
+    {
+        return false;
+    }
+
+    combination[index]++;
+    for (int i = index + 1; i < combination.Length; i++)
+    {
+        combination[i] = combination[i - 1] + 1;
+    }
+
+    return true;
+}
+
+int range = 20;
+int totalCombinations = 0;
+int[] combination = InitializeCombination(6);
+
+do
+{
+    PrintCombination(combination);
+    totalCombinations++;
+}
+while (NextCombination(combination, range));
+
+Console.WriteLine("TotalCombinations: " + totalCombinations);
