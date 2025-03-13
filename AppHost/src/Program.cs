ed0e61d324a61b20ed7557d09ece383ece44c26c
Antonio Santana (from Dev Box) <ansantan@microsoft.com>
2025-03-12 18:29:56 -0700

    Add User Interface project reference and integration
    Updated `AppHost.csproj` to include a reference to `UserInterface.csproj`. Modified `Program.cs` to add the `UserInterface` project to the application builder, enabling its functionality during execution.

--- AppHost/src/Program.cs
+++ AppHost/src/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.CodeSmellDetection>("CodeSmellDetection");
 
+builder.AddProject<Projects.UserInterface>("UserInterface");
+
 builder.Build().Run();
