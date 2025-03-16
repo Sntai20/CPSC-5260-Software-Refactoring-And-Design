a9fcbe02428cc42bb176f8e41424c428984e2f61
Antonio Santana <ansantan@microsoft.com>
2025-03-16 13:53:37 -0700

    Simplify Split method syntax in DuplicatedCodeDetector
    Refactored the `Split` method call on the `content` string to use shorthand syntax `[Environment.NewLine]` instead of `new[] { Environment.NewLine }`. This change enhances code readability while maintaining the same functionality.

--- CodeSmellDetection/src/DuplicatedCodeDetector.cs
+++ CodeSmellDetection/src/DuplicatedCodeDetector.cs
@@ -139,7 +139,7 @@ internal class DuplicatedCodeDetector(
         var content = function.Substring(startIndex, endIndex - startIndex).Trim();
 
         // Normalize indentation.
-        var lines = content.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+        var lines = content.Split([Environment.NewLine], StringSplitOptions.None);
         var trimmedLines = lines.Select(line => line.TrimStart());
         return string.Join(Environment.NewLine, trimmedLines);
     }
