e709de94b8606f8cb3e6e1639b525dc1dc414a35
Antonio Santana <ansantan@microsoft.com>
2025-03-19 13:35:41 -0700

    Add CodeRefactorService using directive
    This commit introduces a new using directive for
    `CodeRefactorService` in `Program.cs`, while keeping
    the existing directives for `Microsoft.Extensions.DependencyInjection`
    and `Microsoft.Extensions.Hosting`. This change indicates
    the program's integration of functionality from the
    `CodeRefactorService` namespace, potentially enhancing
    code refactoring capabilities.

--- CodeRefactorService/src/Program.cs
+++ CodeRefactorService/src/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using CodeRefactorService;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var hostApplicationBuilder = Host.CreateApplicationBuilder(args);
